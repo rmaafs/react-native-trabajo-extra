686ad09232653316a5dee3a7ecde9238e3a5c425
Rodrigo Maafs <rodrigo@ticketfactura.com>
2021-06-12 13:18:33 -0500

    Diseño de primera vista del sidemenu

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import CustomSidebarMenu from "./CustomSidebarMenu";
 
-export default function App() {
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -11,11 +18,117 @@ export default function App() {
   );
 }
 
+function firstScreenStack(props: any) {
+  return (
+    <Stack.Navigator initialRouteName="FirstPage">
+      <Stack.Screen
+        name="FirstPage"
+        component={HomeScreen}
+        options={{
+          title: "Home", //Set Header Title
+          headerLeft: () => (
+            <NavigationDrawerStructure navigationProps={props.navigation} />
+          ),
+          headerStyle: {
+            backgroundColor: "#f4511e", //Set Header color
+          },
+          headerTintColor: "#fff", //Set Header text color
+          headerTitleStyle: {
+            fontWeight: "bold", //Set Header text style
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+const NavigationDrawerStructure = (props: any) => {
+  //Structure for the navigatin Drawer
+  const toggleDrawer = () => {
+    //Props to open/close the drawer
+    props.navigationProps.toggleDrawer();
+  };
+
+  return (
+    <View style={{ flexDirection: "row" }}>
+      <TouchableOpacity onPress={toggleDrawer}>
+        {/*Donute Button Image */}
+        <Image
+          source={{
+            uri: "https://raw.githubusercontent.com/AboutReact/sampleresource/master/drawerWhite.png",
+          }}
+          style={{ width: 25, height: 25, marginLeft: 5 }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+function secondScreenStack(props: any) {
+  return (
+    <Stack.Navigator
+      initialRouteName="SecondPage"
+      screenOptions={{
+        headerLeft: () => (
+          <NavigationDrawerStructure navigationProps={props.navigation} />
+        ),
+        headerStyle: {
+          backgroundColor: "#f4511e", //Set Header color
+        },
+        headerTintColor: "#fff", //Set Header text color
+        headerTitleStyle: {
+          fontWeight: "bold", //Set Header text style
+        },
+      }}
+    >
+      <Stack.Screen
+        name="SecondPage"
+        component={HomeScreen}
+        options={{
+          title: "Second Page", //Set Header Title
+        }}
+      />
+      <Stack.Screen
+        name="ThirdPage"
+        component={HomeScreen}
+        options={{
+          title: "Third Page", //Set Header Title
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        drawerContentOptions={{
+          activeTintColor: "#e91e63",
+          itemStyle: { marginVertical: 5 },
+        }}
+        drawerContent={(props) => <CustomSidebarMenu {...props} />}
+      >
+        <Drawer.Screen
+          name="FirstPage"
+          options={{ drawerLabel: "First page Option" }}
+          component={firstScreenStack}
+        />
+        <Drawer.Screen
+          name="SecondPage"
+          options={{ drawerLabel: "Second page Option" }}
+          component={secondScreenStack}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
